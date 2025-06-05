d1d9047276461044a7949aa53bb19e65377451fd
laszlocseh <csehlaszlo91@gmail.com>
2025-06-05 15:30:47 +0300

    init plone 6

--- calculate_next_release.sh
+++ calculate_next_release.sh
@@ -1,1 +1,1 @@
-major=$(grep FROM Dockerfile | awk -F':|-' '{print $2}');let minor=$(git tag | grep -E $major-[0-9]+ | cut -d- -f2 | sort -n | tail -n 1 )+1; echo $major-$minor
+major=$(grep 'FROM .*:.*' Dockerfile | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+');let minor=$(git tag | grep -E $major-[0-9]+ | cut -d- -f2 | sort -n | tail -n 1 )+1; echo $major-$minor
